7550976255c093fbf328d8831fcf06323d6a794d
Lukas Steiner <lukas.steiner@steinheilig.de>
2021-09-17 16:02:00 +0200

    add network port outputs

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,11 @@ output "rancher_admin_password" {
   value     = random_password.rancher_admin_password.result
   sensitive = true
 }
+
+output "rancher_server_port_id" {
+  value = openstack_compute_instance_v2.rancher_server.network.0.port
+}
+
+output "rancher_server_ip" {
+  value = openstack_compute_instance_v2.rancher_server.network.0.fixed_ip_v4
+}
