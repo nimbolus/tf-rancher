6b020dccafe16dd3291da1d06657dc40be486c6b
Lukas Steiner <lukas.steiner@steinheilig.de>
2023-02-09 20:15:05 +0100

    remove fixed versions

--- service-account/versions.tf
+++ service-account/versions.tf
@@ -2,7 +2,6 @@ terraform {
   required_providers {
     rancher2 = {
       source  = "rancher/rancher2"
-      version = "1.24.0"
     }
     random = {
       source = "hashicorp/random"
--- versions.tf
+++ versions.tf
@@ -1,21 +1,17 @@
 terraform {
-  required_version = ">= 0.14.0"
+  required_version = ">= 1.0.0"
   required_providers {
     openstack = {
       source  = "terraform-provider-openstack/openstack"
-      version = ">= 1.43.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.1.2"
     }
     k8sbootstrap = {
       source  = "nimbolus/k8sbootstrap"
-      version = ">= 0.1.1"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
-      version = ">= 1.11.1"
     }
   }
 }
