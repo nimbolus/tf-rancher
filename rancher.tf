3faa11ec7f3e9627e15b9e325d2f52bc3ce7b1f4
Lukas Steiner <lukas.steiner@steinheilig.de>
2023-05-20 13:37:08 +0200

    upgrade cert-manager to v1.11.0

--- rancher.tf
+++ rancher.tf
@@ -71,7 +71,7 @@ resource "helm_release" "cert_manager" {
   chart      = "cert-manager"
   name       = "cert-manager"
   namespace  = kubernetes_namespace.cert_manager.metadata.0.name
-  version    = "v1.6.1"
+  version    = "v1.11.0"
   values = [<<-EOT
     installCRDs: true
     extraArgs:
